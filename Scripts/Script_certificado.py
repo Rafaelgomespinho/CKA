1c63827ebaf6181f4e1354ee3dccea607479e083
rafael <rafaelgomes.pinho@hotmail.com>
2021-06-10 14:39:55 -0300

    criado bloco para escolher geraçao de kubelet separado

--- Scripts/Script_certificado.py
+++ Scripts/Script_certificado.py
@@ -58,51 +58,64 @@ while True:
 # 11970623
 # 50 anos = 18250 dias
 # Montar um while para repetir a execução ate o fim do CSV
+opcao=input("Para Gerar certificado digite 1 ou 2 para gerar o KUBECTL: ")
+if opcao == '1':
+    while a <= cont1:
+        if Path(diretorio + "/" + lista_if(a)[1] + "/" + ambiente).is_dir():
+            print("Diretorio já existe")
+            a += 1
+        else:
+            print("Criando Diretorio")
+            os.system("mkdir -p " + diretorio + "/" +
+                    lista_if(a)[1] + "/" + ambiente)
+            a += 1
 
-while a <= cont1:
-    if Path(diretorio + "/" + lista_if(a)[1] + "/" + ambiente).is_dir():
-        print("Diretorio já existe")
-        a += 1
-    else:
-        print("Criando Diretorio")
-        os.system("mkdir -p " + diretorio + "/" +
-                  lista_if(a)[1] + "/" + ambiente)
-        a += 1
+
+    while i <= cont:
+
+        diretorio1 = diretorio + "/"+lista_if(i)[1] + "/" + ambiente + "/"
+        password = str(lista_if(i)[1].capitalize() + "@123")
+        cliente = str(lista_if(i)[1])
+        ispb = str(lista_if(i)[0])
+        namespace = str(lista_if(i)[3])
+        
+        print("Criando a Key Digite a senha desejada")
+        os.system("openssl genrsa -des3 -passout pass:" + password +
+                " -out " + diretorio1 + cliente + "_" + ispb + ".key 2048")
+
+        print("Criando Chave Publica CRT")
+
+        os.system("openssl req -passin pass:" + password + " -new -x509 -nodes -sha1 -days " + Vencimento + " -key " +
+                diretorio1 + cliente + "_" + ispb + ".key > " + diretorio1 + cliente + "_" + ispb + ".crt -subj '/C=BR/ST=Sao paulo/L=Sao paulo/O="+cliente+"/OU="+cliente+ambiente+"/CN=www."+cliente+".com'")
+
+        os.system("openssl rsa -passin pass:" + password + " -in " + diretorio1 + cliente + "_" + ispb +
+                ".key -out " + diretorio1 + "no.pwd." + cliente + "_" + ispb + ".key")
+
+        #print("Criando Backup ")
+        # os.system("cp "+diretorio+"/"+cliente+"_"+ispb+".key " +
+        #         diretorio+"/"+cliente+"_"+ispb+".key_backup")
+
+        print("Exportando p12")
+
+        os.system("openssl pkcs12 -export -passout pass:" + password+" -in " + diretorio1 + cliente + "_" + ispb + ".crt -inkey " + diretorio1 +
+                "no.pwd." + cliente + "_" + ispb + ".key -out " + diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente + ".pfx")
+
+        
+        i += 1
+   
+if opcao == '2':
+            while i <= cont:
+                diretorio1 = diretorio + "/"+lista_if(i)[1] + "/" + ambiente + "/"
+                cliente = str(lista_if(i)[1])
+                ispb = str(lista_if(i)[0])
+                namespace = str(lista_if(i)[3])
 
 
-while i <= cont:
+                print("Gerando script Kubectl")
+                os.system("echo kubectl create secret generic certificate-"+ambiente+" --from-file="+diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente +"="+diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente + ".pfx --from-file=" + diretorio1 + cliente + "_" + ispb + ".crt=" + diretorio1 + cliente + "_" + ispb + ".crt -n "+namespace+" >> /tmp/kubectl.txt")
+                i+=1
+                print('Fim da Execução do kubelet')
 
-    diretorio1 = diretorio + "/"+lista_if(i)[1] + "/" + ambiente + "/"
-    password = str(lista_if(i)[1].capitalize() + "@123")
-    cliente = str(lista_if(i)[1])
-    ispb = str(lista_if(i)[0])
-    namespace = str(lista_if(i)[3])
     
-    print("Criando a Key Digite a senha desejada")
-    os.system("openssl genrsa -des3 -passout pass:" + password +
-              " -out " + diretorio1 + cliente + "_" + ispb + ".key 2048")
-
-    print("Criando Chave Publica CRT")
-
-    os.system("openssl req -passin pass:" + password + " -new -x509 -nodes -sha1 -days " + Vencimento + " -key " +
-              diretorio1 + cliente + "_" + ispb + ".key > " + diretorio1 + cliente + "_" + ispb + ".crt -subj '/C=BR/ST=Sao paulo/L=Sao paulo/O="+cliente+"/OU="+cliente+ambiente+"/CN=www."+cliente+".com'")
-
-    os.system("openssl rsa -passin pass:" + password + " -in " + diretorio1 + cliente + "_" + ispb +
-              ".key -out " + diretorio1 + "no.pwd." + cliente + "_" + ispb + ".key")
-
-    #print("Criando Backup ")
-    # os.system("cp "+diretorio+"/"+cliente+"_"+ispb+".key " +
-    #         diretorio+"/"+cliente+"_"+ispb+".key_backup")
-
-    print("Exportando p12")
-
-    os.system("openssl pkcs12 -export -passout pass:" + password+" -in " + diretorio1 + cliente + "_" + ispb + ".crt -inkey " + diretorio1 +
-              "no.pwd." + cliente + "_" + ispb + ".key -out " + diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente + ".pfx")
-
-    print("Gerando script Kubectl")
-    os.system("echo kubectl create secret generic certificate-"+ambiente+" --from-file="+diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente +"="+diretorio1 + cliente + "_" + ispb + "_" + dtvenc + "_" + ambiente + ".pfx --from-file=" + diretorio1 + cliente + "_" + ispb + ".crt=" + diretorio1 + cliente + "_" + ispb + ".crt -n "+namespace+" >> /tmp/kubectl.txt")
-
-    i += 1
-
 
 print("Fim da execução")
